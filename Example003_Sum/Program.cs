5413f7291e443d52c98875fcf82b458f4d13b73f
Yuras <monamurs@gmail.com>
2022-08-21 21:01:42 +1000

    Add Random.Next

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int NumberA = 3;
-int NumberB = 31;
+﻿int NumberA = new Random().Next(1, 10); // случайное число от 1 до 9 включительно
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1, 10);
+Console.WriteLine(NumberB);
 Console.WriteLine(NumberA + NumberB);
